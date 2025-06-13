7409b40c6a9a0243eb13ea0dec499a6182d15fb9
Gregory Huet <ghuet2@myges.fr>
2025-06-13 09:57:53 +0200

    J'ai ajouté Fraction fonctionnel et j'ai simplifié ma méthode PGCD

---
+++ TP2_Math/Fraction.java
@@ -0,0 +1,41 @@
+public class Fraction {
+    private final int num; // numérateur
+    private final int den; // dénominateur (>0)
+
+    public Fraction(int num, int den) {
+        assert den!=0:"Le dénominateur ne peut pas etre nul";
+        if (den<0){
+            num=-num;
+            den=-den;
+        }
+        int pgcd=MathsUtils.PGCD(num, den);
+        this.num=num/pgcd;
+        this.den=den/pgcd;
+    }
+    public Fraction add(Fraction a){
+        int nouveauNum = this.num * a.den + a.num * this.den;
+        int nouveauDen = this.den * a.den;
+        return new Fraction(nouveauNum, nouveauDen);
+    }
+    public Fraction multiply(Fraction a){
+        int nouveauNum = this.num * a.num;
+        int nouveauDen = this.den * a.den;
+        return new Fraction(nouveauNum, nouveauDen);
+    }
+    public double toDouble() {
+        return (double) num / den;
+    }
+    @Override
+    public String toString() {
+        return num + "/" + den;
+    }
+
+    public static void main(String[] args) {
+        Fraction f = new Fraction(2, 3);
+        Fraction g = new Fraction(1, 6);
+        System.out.println(f.add(g).toString());
+        System.out.println(f.multiply(g).toString());
+        assert f.add(g).toString().equals("5/6"):"C'est pas le bon résultat";
+        assert f.multiply(g).toString().equals("1/9"):"C'est pas le bon résultat";
+    }
+}
--- TP2_Math/MathsUtils.java
+++ TP2_Math/MathsUtils.java
@@ -1,31 +1,16 @@
 public class MathsUtils {
     public static int PGCD(int a, int b){
-        int i = 1;
-        if (a>=b){
-            i = b;
-            while (i>0) {
-                i--;
-                if (b%i==0){
-                    if (a%i==0){
-                        return i;
-                    }
-                }
-            }
-        }
-        else{
-            i=a;
-            while (i>0) {
-                i++;
-                if (b%i==0){
-                    if (a%i==0){
-                        return i;
-                    }
-                }
-            }
-        }
-        i=0;
-        assert i !=0;
-        return i;
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (a == 0) return b;
+    if (b == 0) return a;
+
+    while (b != 0){
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
     }
     public static void main(String[] args) {
         System.out.println(PGCD(13,50));
